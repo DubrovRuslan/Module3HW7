da10ae8b3ee1449333400aabb4a497b530a12691
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-07-12 22:02:05 +0300

    Done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Module3HW7;
+using Module3HW7.Services;
+using Module3HW7.Services.Abstractions;
+
+var serviceProvider = new ServiceCollection()
+                .AddTransient<IConfigService, ConfigService>()
+                .AddSingleton<IBackupService, BackupService>()
+                .AddSingleton<ILoggerService, LoggerService>()
+                .AddTransient<IFileService, FileService>()
+                .AddTransient<Starter>()
+                .BuildServiceProvider();
+var start = serviceProvider.GetService<Starter>();
+start.Run();
+Console.WriteLine("Done");
+Console.ReadKey();
